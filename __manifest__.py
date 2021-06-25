8bca2acd43517cdc50b328f1c9cfc579eddc4e24
Saly Abbo <elmaroufa@gmail.com>
2021-06-25 22:19:38 +0100

    resolve bg for folder security

--- __manifest__.py
+++ __manifest__.py
@@ -24,8 +24,8 @@
 
     # always loaded
     'data': [
-        'security/ir.model.access.csv',
         'security/security.xml',
+        'security/ir.model.access.csv',
         'views/openacademy.xml',
         'views/templates.xml',
     ],
